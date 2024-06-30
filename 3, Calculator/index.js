baa0780ff841d87df56c507d2e204f9356566273
Sami Al Zaber <68608214+Sami-bd@users.noreply.github.com>
2024-06-30 22:11:08 +0600

    Add files via upload

---
+++ 3, Calculator/index.js
@@ -0,0 +1,27 @@
+
+
+let outputScreen=document.querySelector('#output-screen');
+
+function display(num){
+    outputScreen.value += num;
+}
+
+function Calculate(){
+    try {
+        outputScreen.value = eval(outputScreen.value);
+    }
+
+    catch(err)
+    {
+        outputScreen.value = 'Error..';
+    }
+}
+
+function Clear(){
+    outputScreen.value = "";
+}
+
+function del()
+{
+    outputScreen.value = outputScreen.value.slice(0, -1);
+}
